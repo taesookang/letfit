582f14530d2a1e1d360214a06cc77835c91af00e
Taesoo Kang <tarrak123@gmail.com>
2021-06-02 01:47:38 -0400

    EDIT: section offset value

--- js/main.js
+++ js/main.js
@@ -17,7 +17,7 @@ $(document).ready(function () {
     $("section").each(function () {
       let top = $(window).scrollTop();
       let height = $(this).height();
-      let offset = $(this).offset().top - 200;
+      let offset = $(this).offset().top - 400;
       let id = $(this).attr("id");
 
       if (top >= offset && top < offset + height) {
